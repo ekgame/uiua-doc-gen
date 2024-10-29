9f7c3ab75f864c7fc0013bcfadb256ca3cb55dad
Ernestas Kluonis <ekgame1@gmail.com>
2024-10-29 07:23:09 +0200

    We actually outputting something now

--- extractor/src/main.rs
+++ extractor/src/main.rs
@@ -1,12 +1,20 @@
 
 use std::env;
+use std::fs::canonicalize;
 use std::path::Path;
+use serde_json::Map;
+use serde_json::Value;
 use uiua::ast::Item;
+use uiua::ast::Word;
 use uiua::Assembly;
 use uiua::BindingInfo;
+use uiua::BindingKind;
 use uiua::CodeSpan;
 use uiua::Compiler;
+use uiua::DocCommentSig;
 use uiua::NativeSys;
+use uiua::Signature;
+use uiua::Sp;
 use uiua::{parse, InputSrc};
 
 fn get_binding_info(asm: &Assembly, span: &CodeSpan) -> Option<BindingInfo> {
@@ -16,8 +24,98 @@ fn get_binding_info(asm: &Assembly, span: &CodeSpan) -> Option<BindingInfo> {
         }
         return Some(binding.clone());
     }
+
     panic!("Binding not found for span {:?}", span);
-    None
+}
+
+fn signature_comment_to_object(doc: DocCommentSig) -> Value {
+    let mut inputs = Vec::new();
+    doc.args.iter().for_each(|input|
+        inputs.push(input.name.to_string())
+    );
+
+    let mut outputs = Vec::new();
+    doc.outputs.and_then(|output|
+        Some(output.iter().for_each(|output|
+            outputs.push(output.name.to_string())
+        ))
+    );
+
+    let mut output = Map::new();
+    output.insert("outputs".to_string(), Value::Array(outputs.into_iter().map(Value::String).collect()));
+    output.insert("inputs".to_string(), Value::Array(inputs.into_iter().map(Value::String).collect()));
+    
+    Value::Object(output)
+}
+
+fn format_signature(signature: Signature) -> Value {
+    let mut output = Map::new();
+    output.insert("inputs".to_string(), Value::Number(signature.args.into()));
+    output.insert("outputs".to_string(), Value::Number(signature.outputs.into()));
+    Value::Object(output)
+}
+
+fn get_words_as_code(words: &Vec<Vec<Sp<Word>>>, asm: &Assembly) -> String {
+    if words.first().unwrap().is_empty() {
+        return "".to_string();
+    }
+
+    let from = &words.first().unwrap().first().unwrap().span;
+    let to = &words.last().unwrap().last().unwrap().span;
+    let span = from.clone().merge(to.clone());
+    span.as_str(&asm.inputs, |code| code.to_owned())
+}
+
+fn handle_ast_items(items: Vec<Item>, asm: &Assembly) -> Vec<Value> {
+    let mut results = Vec::new();
+
+    for item in items {
+        match item {
+            Item::Words(words) => {
+                let code_str = get_words_as_code(&words, asm).replace("\r\n", "\n");
+                let code = code_str.split("\n\n");
+
+                for chunk in code {
+                    let mut output = Map::new();
+                    output.insert("type".to_string(), Value::String("words".to_string()));
+                    output.insert("code".to_string(), Value::String(chunk.to_string()));
+                    results.push(Value::Object(output));
+                }
+            }
+            Item::Binding(binding) => {
+                let mut output = Map::new();
+
+                let info = get_binding_info(asm, &binding.name.span).unwrap();
+                let code = binding.span().as_str(&asm.inputs, |code| code.to_owned());
+                let comment = info.comment.clone().map_or(Value::Null, |comment| Value::String(comment.text.to_string()));
+                let signature = info.comment.and_then(|comment| comment.sig);
+                
+                output.insert("type".to_string(), Value::String("binding".to_string()));
+                output.insert("name".to_string(), Value::String(binding.name.value.to_string()));
+                output.insert("code".to_string(), Value::String(code));
+                output.insert("public".to_string(), Value::Bool(info.public));
+                output.insert("comment".to_string(), comment);
+
+                match info.kind {
+                    BindingKind::Const(value) => {
+                        output.insert("kind".to_string(), Value::String("const".to_string()));
+                        output.insert("value".to_string(), value.map_or(Value::Null, |v| Value::String(v.to_string())));
+                    },
+                    BindingKind::Func(function) => {
+                        output.insert("kind".to_string(), Value::String("func".to_string()));
+                        output.insert("signature".to_string(), format_signature(function.signature));
+                        output.insert("named_signature".to_string(), signature.map_or(Value::Null, signature_comment_to_object));
+                    }
+                    _ => {}
+                }
+
+                results.push(Value::Object(output));
+            }
+            _ => {}
+        }
+    }
+
+    results
 }
 
 fn main() {
@@ -46,10 +144,17 @@ fn main() {
     let mut inputs = asm.inputs.clone();
     let files: Vec<_> = inputs.files.iter()
         .map(|file| (file.key().clone(), file.value().clone())).collect();
+
+    let mut output_files = Vec::new();
     
     for (file_path, file_content) in files {
+        let mut output_file = Map::new();
+
+        let full_file_path = canonicalize(&file_path).unwrap();
         let src = InputSrc::File(file_path.clone().into());
         let (items, errors, _) = parse(&file_content, src, &mut inputs);
+
+        output_file.insert("file".to_string(), serde_json::Value::String(full_file_path.to_string_lossy().into_owned()));
 
         if errors.len() > 0 {
             eprintln!("Error: {} errors found in '{}'", errors.len(), file_path.to_str().unwrap());
@@ -59,18 +164,11 @@ fn main() {
             std::process::exit(1);
         }
 
-        println!("");
-        println!("Bindings in '{}':", file_path.to_str().unwrap());
-
-        for item in items {
-            match item {
-                Item::Binding(binding) => {
-                    println!("{:?}", binding.name);
-                    let info = get_binding_info(&asm, &binding.name.span).unwrap();
-                    println!("{:?}", info);
-                }
-                _ => {}
-            }
-        }
+        let processed_items = handle_ast_items(items, &asm);
+        output_file.insert("items".to_string(), serde_json::Value::Array(processed_items));
+        output_files.push(Value::Object(output_file));
     }
+
+    let output = Value::Array(output_files);
+    println!("{}", serde_json::to_string_pretty(&output).unwrap());
 }
